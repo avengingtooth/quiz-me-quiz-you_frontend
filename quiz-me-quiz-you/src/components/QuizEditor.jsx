ddc7a2f5f524084509da2feef44ae1653258558b
avengingtooth <esellin@gmail.com>
2023-03-09 14:10:15 +0100

    adding questions and answers and saving them is functional

--- quiz-me-quiz-you/src/components/QuizEditor.jsx
+++ quiz-me-quiz-you/src/components/QuizEditor.jsx
@@ -1,38 +1,71 @@
 import { useState } from "react"
-import Answer from "./Answer"
 
 function handleSubmit(e){
     e.preventDefault()
 }
 
-function setAns(e, answers, ind, key){
-    let tempCopy = [...answers]
-    tempCopy[ind][key] = e.target.value
+function addAnswer(questions, qInd, emptyAnswer){
+    let tempCopy = [...questions]
+    tempCopy[qInd].answers.push(emptyAnswer)
+    return tempCopy
+}
+
+function updatedQuestionCopy(e, questions, qInd, aInd, key){
+    let tempCopy = [...questions]
+    console.log(aInd)
+    if(typeof aInd === 'number'){
+        tempCopy[qInd]['answers'][aInd][key] = e.target.value
+    }
+    else{
+        tempCopy[qInd]['title'] = e.target.value
+    }
     return tempCopy
 }
 
 function QuizEditor(props){
     const [title, setTitle] = useState('')
     const [description, setDesciption] = useState('')
-    const [answers, updateAnswers] = useState([])
+    const [questions, updateQuestions] = useState([])
+    const emptyAnswer = {
+        content: '',
+        score: 0
+    }
+    const emptyQuestion = {
+        title: 'title', 
+        answers: [
+            emptyAnswer
+        ]
+    }
 
     return(
         <div className="editor" onSubmit={e => handleSubmit(e)}>
             <form action="">
-                <input type="text" name="title" placeholder='Title' onChange={event => setTitle(event.target.value)}/>
-                <textarea type="text" name="description" placeholder='Descrition' onChange={event => setTitle(event.target.value)}/>
-                <div className="answerContainer">
+                <input className="title" type="text" name="title" placeholder='Title' value={title} onChange={event => setTitle(event.target.value)}/>
+                <textarea type="text" name="description" placeholder='Description' value={description} onChange={event => setDesciption(event.target.value)}/>
+                <div className="questions">
                     {
-                        answers.map((ans, ind) => {
+                        questions.map((q, qInd) => {
                             return(
-                                <div className="answer">
-                                    <input type="text" name="content" value={ans.content} onChange={e => updateAnswers(setAns(e, answers, ind, 'content'))}/>
-                                    <input type="number" name="score" value={ans.score} onChange={e => updateAnswers(setAns(e, answers, ind, 'score'))}/>
+                                <div className="question" key={qInd} >
+                                    <input type="text" placeholder="Question" value={questions[qInd].title} onChange={e => updateQuestions(updatedQuestionCopy(e, questions, qInd))}/>
+                                    <div className="answerContainer">
+                                        {
+                                            q.answers.map((ans, aInd) => {
+                                                return(
+                                                    <div key={aInd} className="answer">
+                                                        <input type="text" name="content" value={questions[qInd].answers[aInd].content} onChange={e => updateQuestions(updatedQuestionCopy(e, questions, qInd, aInd, 'content'))}/>
+                                                        <input type="number" name="score" value={ans.score} onChange={e => updateQuestions(updatedQuestionCopy(e, questions, qInd, aInd, 'score'))}/>
+                                                    </div>
+                                                )
+                                            })
+                                        }      
+                                        <button className="addAns" onClick={() => updateQuestions(addAnswer(questions, qInd, emptyAnswer))}>Add Answer</button>
+                                    </div>
                                 </div>
                             )
                         })
-                    }      
-                    <button onClick={() => {updateAnswers([...answers, {content:'', score:0}])}}>Add Answer</button>
+                    }
+                    <button onClick={() => {updateQuestions([...questions, emptyQuestion])}}>Add Question</button>
                 </div>
                 <button>{props.action}</button>
             </form>
