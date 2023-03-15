c269cbbba904c13d0d2558c8c46249bd76ca6021
TheMike1972 <TheMike1972@github.com>
2023-03-15 11:45:35 +0100

    game play beginning

---
+++ src/components/Result.jsx
@@ -0,0 +1,13 @@
+// import React, { useState } from 'react'
+
+
+// const Result = () => {
+//     const [score, setScore] = useState(0);
+//     const handleClick() = 
+
+//     return (
+//         <div>Score: {score}</div>
+//     )
+// }
+
+// export default Result
