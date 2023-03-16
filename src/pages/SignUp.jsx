1e7776859509515c0fd4e17cebb3ef8655e08bf1
Mershouta <jchristkay@gmail.com>
2023-03-16 17:03:26 +0100

    redirect

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react"
 import EssentialAuth from "../components/EssentialAuth"
 import myApi from "../service/api"
+import { useNavigate } from 'react-router-dom'
+
 
 
 function checkOverall(fields, updateOverallValidity) {
@@ -22,6 +24,7 @@ function SignUp() {
     const [validPsw, updatePswValidity] = useState(false)
     const [uniqueUsername, updateUniqueUsername] = useState(false)
     const [allFieldsValid, updateOverallValidity] = useState(false)
+    const navigate = useNavigate
 
     // changes the classname depending on if password is valid or not
     // add client side checks for password secure enought before allowing to validate 
@@ -40,14 +43,19 @@ function SignUp() {
         }
     }, [confirmPassword, password])
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(username + ' ' + password)
-        if (password !== confirmPassword) {
-            return
+    async function handleSubmit(event) {
+        event.preventDefault()
+        const userToCreate = { username, password }
+    
+        try {
+          const response = await myApi.post('/auth/signup', userToCreate)
+          if (response.status === 201) {
+            navigate('/login')
+          }
+        } catch (error) {
+          console.error(error)
         }
-        myApi.post('/auth/signup', { username, password })
-    }
+      }
 
 
     // check if username is unique in db
